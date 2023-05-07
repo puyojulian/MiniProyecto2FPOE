e77d7caf4a75ae655c2a2fc732ce70b3be0640a6
Sebastián Orrego <orrego.sebastian@correounivalle.edu.co>
2023-05-06 22:58:10 -0500

    LightButtons de VistaEstadistica implementados

--- src/co/edu/univalle/miniproyecto2/view/VistaInicio.java
+++ src/co/edu/univalle/miniproyecto2/view/VistaInicio.java
@@ -8,19 +8,14 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-
 import java.applet.AudioClip;
 import java.awt.Image;
 import javax.swing.ImageIcon;
-import javax.swing.JToggleButton;
 /**
  *
  * @author Sebastián
@@ -110,7 +105,7 @@ public class VistaInicio extends JFrame{
         }
     }
     
-    class ManejadorDeEventos implements ActionListener, KeyListener{
+    class ManejadorDeEventos implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent evento){
             if(evento.getSource() == btnJugar){                
@@ -121,24 +116,6 @@ public class VistaInicio extends JFrame{
                 Musica = !Musica;
                 Musica(Musica);
             }
-        }
-        
-        @Override
-        public void keyReleased(KeyEvent e) {
-           /* System.out.println("Se liberó la tecla " + e.getKeyChar() +
-                    " Con codigo " + e.getKeyCode());*/
-        }
-        
-        @Override
-        public void keyPressed(KeyEvent e) {
-            /*System.out.println("Se presionó la tecla " + e.getKeyChar()+
-                    " Con codigo " + e.getKeyCode());*/
-        }
-        
-        @Override
-        public void keyTyped(KeyEvent e) {
-            /*System.out.println("Se digitó la tecla " + e.getKeyChar()+
-                    " Con codigo " + e.getKeyCode());*/
         }
     }
     
